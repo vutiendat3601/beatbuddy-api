ece4848274c9c085eeca3c7d7f4829b0b3a98ce8
vutiendat3601 <vutien.dat.3601@gmail.com>
2024-06-08 00:09:59 +0700

    feat: remove text accent

--- src/main/java/vn/io/vutiendat3601/beatbuddy/api/util/StringUtils.java
+++ src/main/java/vn/io/vutiendat3601/beatbuddy/api/util/StringUtils.java
@@ -1,8 +1,27 @@
 package vn.io.vutiendat3601.beatbuddy.api.util;
 
+import java.text.Normalizer;
+import java.text.Normalizer.Form;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.function.Supplier;
+
 public class StringUtils {
   private static final String ALPHABET =
       "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  private static final Supplier<Map<Character, Character>> UNACCENT_CHARACTERS_MAP_SUPPLIER =
+      () -> {
+        final String CHARACTERS = "Đđ";
+        final String UNACCENT_CHARACTERS = "Dd";
+        final Map<Character, Character> result = new HashMap<>();
+        for (int i = 0; i < CHARACTERS.length(); i++) {
+          result.put(CHARACTERS.charAt(i), UNACCENT_CHARACTERS.charAt(i));
+        }
+        return result;
+      };
+  private static final Map<Character, Character> UNACCENT_CHARACTERS_MAP =
+      UNACCENT_CHARACTERS_MAP_SUPPLIER.get();
 
   public static String makeRandomString(int length) {
     StringBuilder sb = new StringBuilder(length);
@@ -11,5 +30,13 @@ public class StringUtils {
       sb.append(ALPHABET.charAt(index));
     }
     return sb.toString();
+  }
+
+  public static String removeAccent(String text) {
+    String result = Normalizer.normalize(text, Form.NFD).replaceAll("\\p{M}", "");
+    for (Entry<Character, Character> cEntry : UNACCENT_CHARACTERS_MAP.entrySet()) {
+      result = result.replace(cEntry.getKey(), cEntry.getValue());
+    }
+    return result.toString();
   }
 }
